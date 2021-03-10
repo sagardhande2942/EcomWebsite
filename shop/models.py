4b99edc2cd598ef68a6d8cd26e2cb6f8db03eec2
Sagar <dhandesagar79@gmail.com>
2021-03-11 02:13:50 +0530

    Initial Commit

---
+++ shop/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_id = models.AutoField(primary_key=True)
+    product_desc = models.CharField(max_length=500)
+    product_pubs_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="shop/images", default="")
+
+    def __str__(self):
+        return self.product_name
---
+++ shop/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nslides =   n//4 +  ceil(n/4 - n//4)
+    allprods = []
+    catprods = Product.objects.values('category', 'product_id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n//4 + ceil(n/4 - n//4)
+        allprods.append([prod, range(1, nslides), nslides])
+
+    # allProds = [
+    #     [products, range(1, nslides), nslides],
+    #     [products, range(1, nslides), nslides]
+        
+    # ]
+    param = {
+        'allprods': allprods
+    }
+    
+    return render(request, 'shop/index.html', param)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def productView(request, myid):
+    product = Product.objects.filter(product_id = myid)
+    param = {}
+    param['prod'] =  product
+    return render(request, 'shop/products.html', param)
+
+
+def checkout(request):
+    return HttpResponse("Hii you are in checkout")
+
+
+def search(request):
+    return HttpResponse("Hii you are in search")
